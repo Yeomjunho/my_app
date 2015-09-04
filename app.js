dd8e4ef9f2333f3ef42d01acb82fc859b025b628
Yeomjunho <junhoda1004@naver.com>
2015-09-04 11:18:55 +0900

    public

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require('express'),
+	path = require('path'),
 	app = express();
 
-app.get('/',function(req,res){
-	res.send('hello world');
-});
+app.use(express.static(path.join(__dirname,'public')));
+console.log(__dirname);
+
+/*app.get('/',function(req,res){
+	res.send('hello world');	
+});*/
 
 app.listen(3000,function(){
 	console.log('seve on');
